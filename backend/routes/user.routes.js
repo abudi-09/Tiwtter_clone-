3c14c9d8f0690bc5494bb436ccc9c31af78c0d7b
abudi-09 <Abdsalih229@gmail.com>
2025-06-27 16:15:43 +0300

    suggetsed , update profile

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,15 +2,14 @@ import express from "express";
 import {
   getUserProfile,
   followUnfollowUser,
+  getSuggestedUsers, // ensure this is imported correctly
   // updateUserProflie (uncomment this line if you plan to use it)
+  updateUser, // ensure this is imported correctly
 } from "../controllers/user.controller.js";
 import { protectRoute } from "../middleware/protectRoutes.js";
-
 const router = express.Router();
-
 router.get("/profile/:username", protectRoute, getUserProfile);
-// router.get("/suggested", protectRoute, getUserProfile); // Uncomment when ready
+router.get("/Suggested", protectRoute, getSuggestedUsers); // Uncomment and ensure it's imported
 router.post("/followers/:id", protectRoute, followUnfollowUser);
-// router.get("/update", protectRoute, updateUserProflie); // Uncomment and ensure it's imported
-
+router.post("/update", protectRoute, updateUser); // Uncomment and ensure it's imported
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
 dotenv.config();
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
@@ -7,11 +8,17 @@ import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+cloudinary.config({
+  cloud_name: process.env.cloudinary_cloud_name,
+  api_key: process.env.cloudinary_api_key,
+  api_secret: process.env.cloudinary_api_secret,
+});
 const PORT = process.env.PORT || 5000;
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
   connectMongoDB();
 });
